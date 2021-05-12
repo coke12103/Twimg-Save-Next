8ceb30c258c14e3b833ec2418a83d559ccbe4fa1
coke12103 <coke12103@gmail.com>
2021-05-12 16:45:21 +0900

    add plugin api

---
+++ src/main/clay/api.js
@@ -0,0 +1,39 @@
+const download = require('./download.js');
+
+module.exports = class Clay{
+  constructor(clay_core){
+    this.core = clay_core;
+  }
+
+  trap(data){
+    var is_followed = false;
+
+    console.log(`start trap!\n  code: ${data.code}`);
+
+    for(var spell of this.core.spells){
+      if(!(spell.type && spell.type.follow) || spell.type.follow.code != data.code) continue;
+
+      var follow = spell.type.follow;
+
+      console.log('follow');
+
+      // set_sns_type(follow.target_text);
+
+      this.core.follow[spell.id][follow.exec](data);
+      is_followed = true;
+      break;
+    }
+
+    if(is_followed) return;
+
+    // 通知系の処理
+  }
+
+  set_status_text(text){
+    console.log(text);
+  }
+
+  download(url, filename, save_dir, ref){
+    download(url, filename, save_dir, ref);
+  }
+}
