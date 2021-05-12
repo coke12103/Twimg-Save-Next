3642495c5379f8b7904c4dc979d933d20834c3fc
coke12103 <coke12103@gmail.com>
2021-05-12 17:49:51 +0900

    fix download

--- src/main/clay/download.js
+++ src/main/clay/download.js
@@ -5,7 +5,7 @@ module.exports = async function download(url, filename, save_dir, ref){
   var opt = {
     url: url,
     method: 'GET',
-    encoding: null,
+    responseType: 'buffer',
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.72 Safari/537.36'
     }
@@ -15,16 +15,17 @@ module.exports = async function download(url, filename, save_dir, ref){
 
   try{
     var body = await got(opt);
+    body = body.body;
   }catch(err){
     // set_status_text(`Download: ${err.statusCode}`);
     console.log(err);
     throw err;
   }
 
-  console.log(`Download: OK`);
+  console.log('Download: OK');
 
   try{
-    fs.writeFileSync(`${save_dir}/${name}`, body, { encoding: 'binary' });
+    fs.writeFileSync(`${save_dir}/${filename}`, body, { encoding: 'binary' });
   }catch(err){
     console.log(err);
     throw err;
