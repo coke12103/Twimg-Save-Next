bfc254a00a7b7580c01288b043555e673fb7be57
coke12103 <coke12103@gmail.com>
2021-05-13 19:54:26 +0900

    add card component

---
+++ src/components/ui/card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ui-card">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="less" scoped>
+  .ui-card{
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    position: relative;
+    background: #fff;
+    box-shadow: 0 3px 8px rgba(0,0,0,.2);
+  }
+</style>
