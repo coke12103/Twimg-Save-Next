61de45f9a59f8aebfbed4ea3a5e2e5cf809e41fc
coke12103 <coke12103@gmail.com>
2021-05-14 21:29:08 +0900

    fix design

--- src/components/ui/card.vue
+++ src/components/ui/card.vue
@@ -7,10 +7,19 @@
 <style lang="less" scoped>
   .ui-card{
     display: flex;
-    flex-direction: column;
     overflow: hidden;
     position: relative;
     background: #fff;
     box-shadow: 0 3px 8px rgba(0,0,0,.2);
+    padding: 12px;
+    width: 90%;
+    align-items: center;
+
+    *{
+      padding-top: 0px;
+      padding-bottom: 0px;
+      padding-left: 8px;
+      padding-right: 8px;
+    }
   }
 </style>
