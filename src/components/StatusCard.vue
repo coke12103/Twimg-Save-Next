331d91a923e9c31e6810b528988b8582a4de17b4
coke12103 <coke12103@gmail.com>
2021-05-15 19:06:43 +0900

    add status text

--- src/components/StatusCard.vue
+++ src/components/StatusCard.vue
@@ -1,5 +1,28 @@
 <template>
-  <ui-card>
-    test
+  <ui-card class="status-card">
+    Status: <span class="status-text">{{status_text}}</span>
   </ui-card>
 </template>
+
+<style lang="less" scoped>
+  .status-card{
+    font-size: 14px;
+    color: rgba(41, 47, 51, 0.7);
+
+    .status-text{
+      font-size: 14px;
+      color: #000;
+      margin-left: 5px;
+    }
+  }
+</style>
+
+<script>
+export default{
+  computed:{
+    status_text(){
+      return this.$store.state.status_text;
+    }
+  }
+}
+</script>
