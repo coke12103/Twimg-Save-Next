cc90e63bf4d755dbab8826808f60f2147fa808c3
coke12103 <coke12103@gmail.com>
2021-05-19 19:03:47 +0900

    add target text display element

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,32 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <StatusCard></StatusCard>
+  <TargetSnsCard></TargetSnsCard>
 </template>
 
 <script>
 import { onMounted } from 'vue';
 import store from './store';
 
-import StatusCard from './components/StatusCard.vue'
+import StatusCard from './components/StatusCard.vue';
+import TargetSnsCard from './components/TargetSnsCard.vue';
 
 export default {
   name: 'App',
   components: {
-    StatusCard
+    StatusCard,
+    TargetSnsCard
   },
 
   setup(){
     const setupEventListener = function(){
       window.api.onStatusTextChange(function(text){
-        console.log(text);
+        console.log(`%cstatus text:%c ${text}`, 'color: orange;', '');
         store.commit('set_status_text', text);
+      });
+
+      window.api.onTargetSnsChange(function(text){
+        store.commit('set_target_sns', text);
       });
 
       window.api.onConsoleLog(function(arg){
@@ -35,13 +42,17 @@ export default {
 
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+
+  > *{
+    margin-bottom: 10px;
+  }
 }
 </style>
---
+++ src/components/TargetSnsCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <ui-card class="target-sns-card">
+    ターゲットSNS: <span class="target-sns-text">{{ $store.state.target_sns_text }}</span>
+  </ui-card>
+</template>
+
+<style lang="less" scoped>
+  .target-sns-card{
+    font-size: 14px;
+    color: rgba(41, 47, 51, 0.7);
+    user-select: none;
+
+    .target-sns-text{
+      font-size: 14px;
+      color: #000;
+      margin-left: 5px;
+      user-select: text;
+    }
+  }
+</style>
+
+<script>
+export default{
+  computed:{
+  }
+}
+</script>
