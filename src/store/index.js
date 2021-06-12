eff026a17c235e4ea628b663c9eda20e415713f8
coke12103 <coke12103@gmail.com>
2021-06-12 19:39:16 +0900

    add store value

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,8 @@ export default createStore({
   state: {
     status_text: '起動していません',
     target_sns_text: '',
-    current_url: ''
+    current_url: '',
+    current_category: ''
   },
   mutations: {
     set_status_text(state, text){
@@ -17,6 +18,10 @@ export default createStore({
 
     set_current_url(state, text){
       state.current_url = text;
+    },
+
+    set_current_category(state, text){
+      state.current_category = text;
     }
   },
   actions: {
