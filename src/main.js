cc5cca490576eac112d5f31356474163f3a05dce
coke12103 <coke12103@gmail.com>
2021-06-12 19:38:21 +0900

    add ui select component

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import store from './store/index.js';
 import uiCard from './components/ui/card.vue';
 import uiInput from './components/ui/input.vue';
 import uiButton from './components/ui/button.vue';
+import uiSelect from './components/ui/select.vue';
 
 const app = createApp(App);
 
 app.component('ui-card', uiCard);
 app.component('ui-input', uiInput);
 app.component('ui-button', uiButton);
+app.component('ui-select', uiSelect);
 
 app.use(store);
 
