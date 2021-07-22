35adb03fce4d40166346ef5de6b4607894c15d91
coke12103 <coke12103@gmail.com>
2021-07-22 23:17:47 +0900

    add switch

---
+++ src/components/ui/switch.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container" @click="toggleValue()">
+    <span class="label"><slot></slot></span>
+    <div class="switch-label" :class="{'checked': value}"><span></span></div>
+    <input class="switch" type="checkbox" v-model="valueLocal" >
+  </div>
+</template>
+
+<style lang="less" scoped>
+  .container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    user-select: none;
+    width: 60px;
+
+    .label{
+      width: 100%;
+      text-align: center;
+      display: block;
+      user-select: none;
+      font-size: 6px;
+      padding-bottom: 4px;
+      color: rgba(41, 47, 51, 0.7);
+    }
+
+    .switch{
+      display: none;
+    }
+
+    .switch-label{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      box-sizing: border-box;
+      border: 1px solid #ccc;
+
+      height: 28px;
+      width: 28px;
+
+      background: #eee;
+      transition: .3s;
+
+      span{
+        text-align: center;
+        font-size: 10px;
+        line-height: 28px;
+        font-weight: bold;
+      }
+
+      span:after{
+        content: "OFF";
+        color: #aaa;
+      }
+
+      &.checked{
+        background: #78bd78;
+
+        span:after{
+          content: "ON";
+          color: #fff;
+        }
+      }
+    }
+  }
+</style>
+
+<script>
+export default{
+  props:{
+    value: Boolean,
+  },
+
+  data(){
+    return{
+      valueLocal: this.value
+    }
+  },
+
+  setup(props, { emit }){
+    const toggleValue = () => {
+      console.log(props.value);
+      emit('update:value', !props.value);
+    };
+
+    //onMounted(resetSelect);
+
+    return {
+      toggleValue
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import uiCard from './components/ui/card.vue';
 import uiInput from './components/ui/input.vue';
 import uiButton from './components/ui/button.vue';
 import uiSelect from './components/ui/select.vue';
+import uiSwitch from './components/ui/switch.vue';
 
 const app = createApp(App);
 
@@ -14,6 +15,7 @@ app.component('ui-card', uiCard);
 app.component('ui-input', uiInput);
 app.component('ui-button', uiButton);
 app.component('ui-select', uiSelect);
+app.component('ui-switch', uiSwitch);
 
 app.use(store);
 
