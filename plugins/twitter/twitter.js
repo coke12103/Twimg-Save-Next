42414fbd38611c961d9155413ccd2d7485475630
coke12103 <coke12103@gmail.com>
2022-07-13 18:52:18 +0900

    fix twitter plugin

--- plugins/twitter/twitter.js
+++ plugins/twitter/twitter.js
@@ -1,5 +1,3 @@
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
 const got = require('got');
 
 // TODO notification
@@ -10,28 +8,7 @@ const twitter = async function(url, save_dir){
   // mobile.twitter.comの場合があるがURLの形式変わらんので雑に変換する
   url = url.replace("mobile.", "");
 
-  const opt = {
-    url: url,
-    encoding: 'utf-8',
-    method: 'GET',
-    headers: { 'User-Agent': 'TenchaBot/0.1' }
-  }
-
-  let parse_body;
-  try{
-    const body = await got(opt);
-
-    Clay.set_status_text("Get page: OK");
-
-    const dom = new JSDOM(body.body);
-    parse_body = dom.window.document;
-  }catch(e){
-    Clay.log(e);
-//    notification.basic_error("投稿を取得することができませんでした!");
-    Clay.set_status_text("download error");
-    throw e;
-  }
-
+  // ツイートのIDを取り出す
   const user_id_and_status_id = url.replace("https://twitter.com/", "");
 
   const user_id = user_id_and_status_id.match(/^(.+)(\/status\/)/)[1];
@@ -40,27 +17,41 @@ const twitter = async function(url, save_dir){
   Clay.log(user_id);
   Clay.log(status_id);
 
+  // Tweetの埋め込みの内部で叩かれてるAPIを使う(そうしないと年齢制限とかに引っ掛かるので)
+  // このAPI的にはWindowsのChromeが多分違和感ない。
+  const opt = {
+    url: `https://cdn.syndication.twimg.com/tweet-result?id=${status_id}&lang=en`,
+    encoding: 'utf-8',
+    method: 'GET',
+    headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.34 Safari/537.36' }
+  }
+
+  let body;
+  try{
+    body = await got(opt).json();
+
+    Clay.set_status_text("Get API: OK");
+  }catch(e){
+    Clay.log(e);
+//    notification.basic_error("投稿を取得することができませんでした!");
+    Clay.set_status_text("Get API: error");
+    throw e;
+  }
+
+  // これ無いと画像ないけど動画だとどうなんだろ
+  if(!'photos' in body){
+    Clay.log("no_image_found");
+//    notification.basic_error("画像がみつかりませんでした!");
+    Clay.set_status_text("No image found");
+    throw 'no_image_found';
+  }
+
   let image_count = 0;
-
-  for(const meta_tag of parse_body.querySelectorAll('meta[property="og:image"]')){
-    const media_url = meta_tag.content.replace("large", "orig");
+  for(const photo of body.photos){
+    // origって効くのか知らないけどアクセスできるしこれで生成されるURLは旧実装系の内部で使われてるやつと同じなので気にしない
+    const media_url = photo.url + ":orig";
+    console.log(media_url)
     Clay.log(media_url);
-
-    // profile_imagesがあるってことは画像付いてないってことと解釈する
-    // いつか仕様が変わるかもしれない
-    if(media_url.match(/profile_images/)){
-      Clay.set_status_text("No Image File");
-//      notification.no_file_error();
-      throw 'No Image File';
-    }
-
-    // TODO: video
-    // videoとimageは共存しない
-    if(media_url.match(/tweet_video_thumb|ext_tw_video_thumb/)){
-      Clay.set_status_text("Unsupported media");
-//      notification.unsupported_notification("この添付メディアは現在は対応していません");
-      throw 'Unsupported media';
-    }
 
     const extension = media_url.match(/(\/media\/)(.+)(\.[a-zA-Z0-9]+)(:[a-zA-Z]+)$/)[3];
     const file_name = `tw_${user_id}_${status_id}_image${image_count}${extension}`;
@@ -73,15 +64,7 @@ const twitter = async function(url, save_dir){
       Clay.set_status_text("download error");
       throw e;
     }
-
     image_count++;
-  }
-
-  // 1枚以下なら割と死なのでないよーって出す
-  if(image_count < 1){
-//    notification.basic_error("画像がみつかりませんでした!");
-    Clay.set_status_text("download error");
-    throw 'download error';
   }
 
   Clay.set_status_text('Download done.');
